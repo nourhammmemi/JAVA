74fa2f4d57093d8f2c7acc6622f0a10a83e8c47b
nourhammmemi <119323547+nourhammmemi@users.noreply.github.com>
2023-12-11 21:16:52 +0100

    prosit 9

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.function.Function;
+
+
+public class Main {
+
+    public static void main(String[] args){
+    
+
+
+    }
+}
